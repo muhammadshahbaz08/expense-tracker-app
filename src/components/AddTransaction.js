d9f54e7ed21c725651c783dcff6bfee789088222
muhammadshahbaz08 <muhamadshahbaz17@gmail.com>
2021-08-11 08:05:38 +0500

    Using useRef hook
    Using useRef hook to get the values from Input  feilds, and also clearing the Input feilds on pressing enter or Adding the transaction.

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,23 +1,31 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useRef } from "react";
 import { GlobalContext } from "./context/GlobalState";
 
 export const AddTransaction = () => {
-  const {addTransaction} = useContext(GlobalContext);
+  const { addTransaction } = useContext(GlobalContext);
 
+  // declaring State's
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
+
+  // Using useRef Hook for getting values
+  const descrInput = useRef();
+  const amountInput = useRef();
 
   // handleSubmit
   const handleSubmit = (event) => {
     event.preventDefault();
-
     const newTransaction = {
       id: Math.floor(Math.random() * 100000000),
       text: text,
       amount: +amount, // + sign for converting amount to number.
     };
-
+    
     addTransaction(newTransaction);
+
+    // After the new transaction is added , then the input feilds are reset
+    descrInput.current.value = "";
+    amountInput.current.value = 0;
   };
   return (
     <div>
@@ -29,18 +37,20 @@ export const AddTransaction = () => {
             type="text"
             id="description"
             placeholder="Details of Transaction"
-            value={text}
-            onChange={(event) => setText(event.target.value)}
+            ref={descrInput}
+            onChange={() => setText(descrInput.current.value)}
           />
         </div>
         <div className="form-control">
-          <label htmlFor="transactionamount">Transaction Amount <br/> (negative- expense , positive-income)</label>
+          <label htmlFor="transactionamount">
+            Transaction Amount <br /> (negative- expense , positive-income)
+          </label>
           <input
             type="text"
             id="transactionamount"
             placeholder="Enter Transaction Amount"
-            value={amount}
-            onChange={(event) => setAmount(event.target.value)}
+            ref={amountInput}
+            onChange={() => setAmount(amountInput.current.value)}
           />
         </div>
         <button className="btn">Add Transaction</button>
